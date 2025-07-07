ab93e7f6380ec2f84a3402ab5fe3abd75cc5bea0
Irving1113 <guo0221yifan@126.com>
2025-07-07 14:30:21 +0800

    fix: fix html title

--- docs/conf.py
+++ docs/conf.py
@@ -89,6 +89,7 @@ pygments_style = "sphinx"
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
+html_title = "Triton-distributed documentation"
 html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
